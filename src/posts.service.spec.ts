12652d14a23e3d5484c2bb1a759182f141bceb1d
Alex Kosyakin <smilykap@gmail.com>
2025-01-29 21:54:20 +0700

    add test 3

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,26 +1,29 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
   let newPost: Post;
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
+  it("should add a new post", () => {
     newPost = postsService.create(post);
 
-    expect(newPost.text).toEqual(post.text);
+    expect(newPost.id).toEqual("2");
   });
 
-  it('should find a post', () => {
-    const newPost = postsService.create(post);
-    expect(postsService.find(newPost.id)).toEqual(newPost);
+  it("should find a post", () => {
+    postsService.create(post);
+    let searchPost = postsService.find("2");
+    expect(searchPost?.id).toEqual("2");
+    searchPost = postsService.find("3");
+    expect(searchPost?.id).toBeFalsy();
   });
 });
