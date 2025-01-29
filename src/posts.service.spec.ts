717ddefb1bc0234205c644a55cde14c1b8013429
Alex Kosyakin <smilykap@gmail.com>
2025-01-29 22:02:38 +0700

    add test 4

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,14 +14,19 @@ describe("PostsService", () => {
   });
 
   it("should add a new post", () => {
+    // act
     newPost = postsService.create(post);
 
+    // assert
     expect(newPost.id).toEqual("2");
   });
 
   it("should find a post", () => {
+    // act
     postsService.create(post);
     let searchPost = postsService.find("2");
+
+    // assert
     expect(searchPost?.id).toEqual("2");
     searchPost = postsService.find("3");
     expect(searchPost?.id).toBeFalsy();
